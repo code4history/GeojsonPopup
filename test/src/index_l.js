cf414bf550ef4becf80b585c3bb8222d165ec123
Kohei Otsuka <kochizufan@gmail.com>
2020-07-16 22:40:32 +0900

    Add marker to Leaflets/OpenLayers

--- test/src/index_l.js
+++ test/src/index_l.js
@@ -1,6 +1,14 @@
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+const icon = L.icon({
+  iconUrl: 'assets/jizo.png',
+
+  iconSize:     [35, 48],
+  iconAnchor:   [17, 48],
+  popupAnchor:  [-3, -76]
+});
+
 const map = L.map('map');
 const defaultCenter = [38.889269, -77.050176];
 const defaultZoom = 15;
@@ -11,3 +19,5 @@ const basemap = L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', 
 map.setView(defaultCenter, defaultZoom);
 
 basemap.addTo(map);
+
+L.marker(defaultCenter, {icon: icon}).addTo(map);
--- test/src/index_o.js
+++ test/src/index_o.js
@@ -1,21 +1,57 @@
-import {Map, View} from 'ol';
+import {Map, View, Feature} from 'ol';
+import {Point} from 'ol/geom';
+import {Style, Icon} from 'ol/style';
+import {Vector as sourceVector} from 'ol/source';
+import {Vector as layerVector} from 'ol/layer';
 import TileLayer from 'ol/layer/Tile';
+import {defaults as defaultControls, Attribution} from 'ol/control';
 import XYZ from 'ol/source/XYZ';
 import {fromLonLat} from 'ol/proj';
 import 'ol/ol.css';
 
 const defaultCenter = [-77.050176, 38.889269];
 const defaultZoom = 15;
+const centerCoordinate = fromLonLat(defaultCenter, 'EPSG:3857');
+
+const iconFeature = new Feature({
+  geometry: new Point(centerCoordinate)
+});
+
+const iconStyle = new Style({
+  image: new Icon(({
+    anchor: [17, 48],
+    anchorXUnits: 'pixels',
+    anchorYUnits: 'pixels',
+    src: 'assets/jizo.png'
+  }))
+});
+
+iconFeature.setStyle(iconStyle);
+
+const vectorSource = new sourceVector({
+  features: [iconFeature]
+});
+
+const vectorLayer = new layerVector({
+  source: vectorSource
+});
+
+const attribution = new Attribution({
+  collapsible: false
+});
+
 new Map({
   target: 'map',
   layers: [
     new TileLayer({
       source: new XYZ({
         url: 'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        attribution: '&copy; OpenStreetMap contributors'
+        attributions: '&copy; OpenStreetMap contributors'
       })
-    })
+    }),
+    vectorLayer
   ],
+  controls: defaultControls({attribution: false}).extend([attribution]),
   view: new View({
     center: fromLonLat(defaultCenter, 'EPSG:3857'),
     zoom: defaultZoom
